fac0b62fa3c5230f7a22faaf2bd35089a56bcf9e
Ariel Mordoch <arielmordoch@gmail.com>
2018-08-16 01:50:11 -0400

    Final touch-up to resume page - Using buttons instead of plain text for slide selectors - Styling and JS tweaked accordingly

--- js/slide.js
+++ js/slide.js
@@ -1,15 +1,14 @@
-// Use an Array here because HTMLCollections aren't iteratable in Edge
-let slide_selections = Array.from(document.getElementById('category-tabs').children);
+let slide_selections = Array.from(document.querySelectorAll('#category-tabs button'));
 let slide_active_selection = document.getElementsByClassName('slide-active-selection')[0];
 let active_slide = document.getElementsByClassName('active-slide')[0];
 // temporarily remove active-slide-nav class on hover, focus, and click
 for(let selection of slide_selections) {
-  selection.onmouseover = selection.onfocus = function () {
+  /* selection.onfocusin = function () {
     slide_active_selection.classList.remove('slide-active-selection');
   };
-  selection.onmouseleave = selection.onfocusout = function () {
+  selection.onfocusout = function () {
     slide_active_selection.classList.add('slide-active-selection');
-  };
+  }; */
   // On click, we also need to change the active slide
   selection.onclick = function (event) {
     // slide-active-selection change

87ef49c3780dfc00085c509a05eb96f6f2f140a4
Ariel Mordoch <arielmordoch@gmail.com>
2019-04-10 21:09:42 -0400

    Reindentation, release Spanish, German homepages
    - Reindented all files. Hopefully they're consistent now (in beautify we trust)
    - Added lang_redirect.js to manage redirecting between languages. It should send you to the current page, but in the other language.

--- js/active_page.js
+++ js/active_page.js
@@ -1,7 +1,7 @@
 let page_url = window.location.href;
 // Modify page_url so highlighting works on initial load
 if (window.location.pathname == '/') {
-  page_url += "index.html"
+  page_url += "index.html";
 }
 let nav_as = document.querySelectorAll('.navbar > ul > li > a');
 // Go through each link, comparing href to current page
---
+++ js/lang_redirect.js
@@ -0,0 +1,19 @@
+function lang_redirect(lang) {
+    if (lang instanceof Event) {
+        lang = lang.target.value;
+        document.getElementById('language-select').form.reset();
+    }
+    // Simply redirect to current page (except for resume) in the given language
+    let path = window.location.pathname;
+    if (lang === 'en') {
+        window.location = window.location.href.replace('/de/', '/').replace('/es/', '/');
+    } else if (lang === 'he' || path.includes('resume')) {
+        add_lang_overlay('', lang);
+    } else if (!path.includes('/' + lang + '/')) {
+        if (path.includes('/de/') || path.includes('/es/')) {
+            window.location = window.location.href.replace('/de/', '/' + lang + '/').replace('/es/', '/' + lang + '/');
+        } else {
+            window.location = window.location.origin + '/' + lang + path;
+        }
+    } 
+}

a5b8edb7d70a4a8039e52d957154bfe0c2ddc024
Ariel Mordoch <arielmordoch@gmail.com>
2019-04-15 00:31:20 -0400

    Integrate localization with Jekyll, js, CSS update
    - All localizations and pages are now working with Jekyll.
    - Common strings are now in _data/i18n.yml, but some code is still repeated (homepage).  That's probably unavoidable.
    - Re-routed links to homepage to only point to '/', updated active_page.js to match.
    - Renamed style.css to main.css because it kept getting overwritten  by Jekyll with normalize.css,  I have no clue why but renaming it is just easier.

--- assets/js/active_page.js
+++ assets/js/active_page.js
@@ -1,8 +1,4 @@
 let page_url = window.location.href;
-// Modify page_url so highlighting works on initial load
-if (window.location.pathname == '/') {
-  page_url += "index.html";
-}
 let nav_as = document.querySelectorAll('.navbar > ul > li > a');
 // Go through each link, comparing href to current page
 for (let a of nav_as) {

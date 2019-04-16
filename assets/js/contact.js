6ef0428131b8d5ca1acb1283d64b1dab9c1e34fa
Ariel Mordoch <arielmordoch@gmail.com>
2019-04-16 00:59:36 -0400

    Remove get_eml() to force use of captcha

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -3,61 +3,53 @@ function handle_captcha(tok) {
     let url = "https://amord-process-captcha.herokuapp.com";
     if (fetch) {
         data = {
-            method:'POST',
+            method: 'POST',
             mode: 'cors',
-            headers:{ "Content-Type": "application/json"},
+            headers: {"Content-Type": "application/json"},
             body: JSON.stringify({token: tok})
         };
-        fetch(url, data).then( response => response.json() ).then( 
-            function(response) {
+        fetch(url, data).then(response => response.json()).then(
+            function (response) {
                 if (response.success) {
-                    get_eml();
+                    fetch('https://arielmordoch.com/hidden.json').then(
+                        response => response.json()
+                    ).then(function (eml) {
+                        encrypted = eml.email;
+                        let decoded = '';
+                        for (let i = 0; i < encrypted.length; i++) {
+                            decoded += String.fromCodePoint(encrypted.codePointAt(i) - 1);
+                        }
+                        window.location.assign("mailto:" + decoded);
+                    });
                 }
             }
-         );
+        );
     } else {
         xhr = new XMLHttpRequest();
         xhr.open('POST', 'https://amord-process-captcha.herokuapp.com', true);
         xhr.setRequestHeader("Content-Type", "application/json");
-        
-        xhr.onreadystatechange = function() {
+
+        xhr.onreadystatechange = function () {
             if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
                 if (this.response.success) {
-                    get_eml();
+                    eml = new XMLHttpRequest();
+                    eml.open('GET', 'https://arielmordoch.com/hidden.json');
+                    eml.responseType = 'json';
+                    eml.onload = function (e) {
+                        encrypted = this.response.email;
+                        let decoded = '';
+                        for (let i = 0; i < encrypted.length; i++) {
+                            decoded += String.fromCodePoint(encrypted.codePointAt(i) - 1);
+                        }
+                        window.location.assign("mailto:" + decoded);
+                    }
+                    eml.send();
                 }
             }
         }
 
-        xhr.send(JSON.stringify({token: tok}));
-    }
-}
-
-
-function get_eml() {
-    // Retrieve and decode message from Github
-    if (fetch) {
-        fetch('https://arielmordoch.com/hidden.json').then(
-            response => response.json()
-        ).then(function (eml) {
-            encrypted = eml.email;
-            let decoded = '';
-            for (let i = 0; i < encrypted.length; i++) {
-                decoded += String.fromCodePoint(encrypted.codePointAt(i) - 1);
-            }
-            window.location.assign("mailto:" + decoded);
-        });
-    } else {
-        eml = new XMLHttpRequest();
-        eml.open('GET', 'https://arielmordoch.com/hidden.json');
-        eml.responseType = 'json';
-        eml.onload = function (e) {
-            encrypted = this.response.email;
-            let decoded = '';
-            for (let i = 0; i < encrypted.length; i++) {
-                decoded += String.fromCodePoint(encrypted.codePointAt(i) - 1);
-            }
-            window.location.assign("mailto:" + decoded);
-        }
-        eml.send();
+        xhr.send(JSON.stringify({
+            token: tok
+        }));
     }
 }

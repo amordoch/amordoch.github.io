6e76ad834a66b3e65b2a5cb438e0fab766fce93f
Ariel Mordoch <arielmordoch@gmail.com>
2019-04-16 02:35:54 -0400

    Edge can iterate NodeLists, remove jekyll feed

--- assets/js/slide.js
+++ assets/js/slide.js
@@ -1,14 +1,8 @@
-let slide_selections = Array.from(document.querySelectorAll('#category-tabs button'));
+let slide_selections = document.querySelectorAll('#category-tabs button');
 let slide_active_selection = document.getElementsByClassName('slide-active-selection')[0];
 let active_slide = document.getElementsByClassName('active-slide')[0];
 // temporarily remove active-slide-nav class on hover, focus, and click
 for(let selection of slide_selections) {
-  /* selection.onfocusin = function () {
-    slide_active_selection.classList.remove('slide-active-selection');
-  };
-  selection.onfocusout = function () {
-    slide_active_selection.classList.add('slide-active-selection');
-  }; */
   // On click, we also need to change the active slide
   selection.onclick = function (event) {
     // slide-active-selection change

4d634b959748ef7091cbb9cf4401809d69df874b
Ariel Mordoch <arielmordoch@gmail.com>
2019-05-01 22:35:22 -0400

    More i18n

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,3 +1,5 @@
+---
+---
 // Instead of submitting the form using the usual method, do it with AJAX
 let contactForm = document.getElementById('contact-form');
 contactForm.addEventListener('submit', function (ev) {
@@ -6,6 +8,18 @@ contactForm.addEventListener('submit', function (ev) {
     if (grecaptcha.getResponse()) {
         // Disable error message, if active
         document.querySelector('.error-container').classList.remove('show');
+        // Disable submit button, change text
+        const PAGE_LANG = document.children[0].lang;
+        const submitButton = document.querySelector('#contact-form button[type="submit"]')
+        const initalText = submitButton.innerHTML;
+        submitButton.disable = 'true';
+        if (PAGE_LANG === 'en') {
+            submitButton.innerHTML = '{{ site.data.i18n.en.contactlabels.waittext }}';
+        } else if (PAGE_LANG === 'de') {
+            submitButton.innerHTML = '{{ site.data.i18n.de.contactlabels.waittext }}';
+        } else if (PAGE_LANG == 'es') {
+            submitButton.innerHTML = '{{ site.data.i18n.es.contactlabels.waittext }}';
+        }
         // Get form response, redirect to thank you page
         fetch("https://amord-process-captcha.herokuapp.com/", {
             method: 'POST',
@@ -16,6 +30,8 @@ contactForm.addEventListener('submit', function (ev) {
                 window.location.assign("/thank-you");
             } else {
                 document.querySelector('.error-container').classList.add('show');
+                submitButton.disable = 'false';
+                submitButton.innerHTML = initalText;
             }
         });
     } else {

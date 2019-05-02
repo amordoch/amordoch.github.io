6dcf9e4dcc28b069873382a570f8eeb62b068102
Ariel Mordoch <arielmordoch@gmail.com>
2019-05-02 14:16:14 -0400

    Cleanup lang_redirect.js

--- assets/js/lang_redirect.js
+++ assets/js/lang_redirect.js
@@ -1,19 +1,20 @@
 function lang_redirect(lang) {
     if (lang instanceof Event) {
-        lang = lang.target.value;
-        document.getElementById('language-select').form.reset();
+        lang = this.value;
+        this.form.reset();
     }
     // Simply redirect to current page (except for resume) in the given language
     let path = window.location.pathname;
+    const PAGE_LANG = document.childNodes[1].lang;
     if (lang === 'en') {
-        window.location = window.location.href.replace('/de/', '/').replace('/es/', '/');
+        window.location.assign(path.replace('/' + PAGE_LANG + '/', '/'));
     } else if (lang === 'he' || (lang === 'es' && path.includes('resume'))) {
-        add_lang_overlay('', lang);
-    } else if (!path.includes('/' + lang + '/')) {
-        if (path.includes('/de/') || path.includes('/es/')) {
-            window.location = window.location.href.replace('/de/', '/' + lang + '/').replace('/es/', '/' + lang + '/');
+        add_lang_overlay(null, lang);
+    } else if (lang !== PAGE_LANG) {
+        if (PAGE_LANG !== 'en') {
+            window.location.assign(path.replace('/' + PAGE_LANG + '/', '/' + lang + '/'));
         } else {
-            window.location = window.location.origin + '/' + lang + path;
+            window.location.assign('/' + lang + path);
         }
     } 
 }

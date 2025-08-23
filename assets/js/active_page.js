fe5b0f672a6d7f4c079fa30cafe0043e8673db7f
Ariel Mordoch <arielmordoch@gmail.com>
2025-08-23 16:11:17 -0400

    Replace dropdown CSS with popover API

--- assets/js/active_page.js
+++ assets/js/active_page.js
@@ -13,3 +13,28 @@ if (hint) {
     hint.style.opacity = 1 - Math.min(window.scrollY / window.innerHeight, 1);
   });
 }
+
+// Position nav popover as anchor positioning, overlay CSS attributes are not
+// fully supported yet
+const langMenu = document.querySelector('#lang-menu')
+langMenu.addEventListener('beforetoggle', () => {
+  const parentRect = document.querySelector('.dropdown').getBoundingClientRect();
+  langMenu.style.left = `${parentRect.left}px`;
+  langMenu.style.top = `${parentRect.bottom}px`;
+  langMenu.style.minWidth = `${parentRect.width}px`;
+})
+// Enable hover for desktop; must use JS as well since position is scripted
+if (window.matchMedia("(hover: hover) and (pointer: fine)").matches) {
+  document.querySelector('.dropdown').addEventListener('mouseenter', () => {
+    langMenu.showPopover();
+  });
+  document.querySelector('.dropdown').addEventListener('mouseleave', () => {
+    langMenu.hidePopover();
+  });
+}
+// And collapse on scroll to avoid the menu sticking around
+window.addEventListener('scroll', () => {
+  if (langMenu.matches(":popover-open")) {
+    langMenu.hidePopover();
+  }
+});
